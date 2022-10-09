863ecbcfd165e4dc309129240f1e2c594fae90ab
Tanvir Islam <tanvir.rb@outlook.com>
2022-10-09 20:50:27 +0100

    stack class added with type

---
+++ src/@types/index.d.ts
@@ -0,0 +1,1 @@
+export type StackType = string | number | boolean | Buffer;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-export const Greeter = (name: string) => {
-  return `Hello ${name}`;
-};
+import { StackType } from './@types';
+
+export class Stack {
+  stack: StackType[];
+  constructor() {
+    this.stack = [];
+  }
+
+  get length() {
+    return this.stack.length;
+  }
+
+  push(item: StackType) {
+    return this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.length - 1];
+  }
+
+  size() {
+    return this.length;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  clear() {
+    this.stack.length = 0;
+  }
+}
